9c9f919e498d7cd2abeba4b091d62cc3445babad
Zdscsf <141560373+Zdscsf@users.noreply.github.com>
2026-02-13 21:14:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
 import serial
 import time
-import os
 
-# Détecte automatiquement le port série disponible
-if os.path.exists("/dev/serial0"):
-    port = "/dev/serial0"
-elif os.path.exists("/dev/ttyAMA0"):
-    port = "/dev/ttyAMA0"
-else:
-    raise Exception("Aucun port série trouvé (/dev/serial0 ou /dev/ttyAMA0)")
-
-print(f"Démarrage bridge sur {port}...")
+print("Démarrage bridge sur /dev/ttyS0...")
 
 ser = serial.Serial("/dev/ttyS0", 115200, timeout=1)
 print("Bridge actif")
@@ -20,7 +11,6 @@ while True:
         line = ser.readline().decode().strip()
         print("Reçu:", line)
 
-    # Exemple test automatique LED
     ser.write(b"LED_ON\n")
     print("Envoyé LED_ON")
     time.sleep(5)
@@ -28,4 +18,3 @@ while True:
     ser.write(b"LED_OFF\n")
     print("Envoyé LED_OFF")
     time.sleep(5)
-
