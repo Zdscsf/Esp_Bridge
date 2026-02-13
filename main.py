222fb376062d29bf29183b8e2f38d8c1c8e0dd49
Zdscsf <141560373+Zdscsf@users.noreply.github.com>
2026-02-13 22:12:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -18,11 +18,12 @@ while True:
     try:
         ser.write(b"LED_ON\n")
         print("Envoyé LED_ON", flush=True)
-        time.sleep(0.5)
+        time.sleep(0.2)
 
         ser.write(b"LED_OFF\n")
         print("Envoyé LED_OFF", flush=True)
-        time.sleep(0.5)
+        time.sleep(0.2)
     except Exception as e:
         print("Erreur écriture:", e, flush=True)
+
 
