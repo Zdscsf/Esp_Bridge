c719a42e295c6f8bccafe07df6d8472a326a4f80
Zdscsf <141560373+Zdscsf@users.noreply.github.com>
2026-02-13 21:25:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import serial
 import time
+import sys
 
 ser = serial.Serial("/dev/ttyS0", 115200, timeout=1)
-print("Bridge actif")
+print("Bridge actif", flush=True)
 
 while True:
-    print("Test log")
+    if ser.in_waiting:
+        line = ser.readline().decode().strip()
+        print("Reçu:", line, flush=True)
+
+    ser.write(b"LED_ON\n")
+    print("Envoyé LED_ON", flush=True)
+    time.sleep(5)
+
+    ser.write(b"LED_OFF\n")
+    print("Envoyé LED_OFF", flush=True)
     time.sleep(5)
